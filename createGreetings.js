e1648f7921088f58bd9ac6e25c58036c7dd7e965
Thando <nodwengut@gmail.com>
2019-09-13 07:49:23 +0200

    tested my functions

--- createGreetings.js
+++ createGreetings.js
@@ -1,107 +1,107 @@
-module.exports =  function CreateGreetings() {
-   let greetingsCounter = 0;
-   let name = "";
-   let usersGreeted = {};
-   let greeting = "";
-   let users = [];
-   
-   function setUser(theName) {
-      name = theName
+module.exports = function CreateGreetings() {
+  let greetingsCounter = 0;
+  let name = "";
+  let usersGreeted = {};
+  let greeting = "";
+  let users = [];
 
-      users.push({
-         name,
-         counter: 1
+  function setUser(theName) {
+    name = theName
+
+    users.push({
+      name,
+      counter: 1
+    });
+  }
+
+  function getUser() {
+    return name;
+  }
+
+  function getUsers() {
+    return users;
+  }
+
+  function increaseCounter() {
+    greetingsCounter++;
+  }
+
+  function updateUserCounter(theName) {
+    if (isNameRepeated(theName)) {
+      users.forEach(user => {
+        if (user.name === theName) {
+          user.counter++;
+        }
       });
-   }
+    }
+  }
 
-   function getUser() {
-      return name;
-   }
+  function getUserCounter(theName) {
+    let filteredName = users.filter(option => {
+      return option.name === theName;
+    })
 
-   function getUsers() {
-      return users;
-   }
+    return filteredName[0].counter;
+  }
 
-   function increaseCounter() {
-      greetingsCounter++;
-   }
+  function getGreetingsCounter() {
+    return greetingsCounter;
+  }
 
-   function updateUserCounter(theName) {
-      if(isNameRepeated(theName)) {
-         users.forEach(user => {
-            if(user.name === theName) {
-               user.counter++;
-            }
-         });
+  function setGreeting(lang, theName) {
+    if (lang === "english") {
+      greeting = `Hello, ${theName.charAt(0).toUpperCase() + theName.slice(1)}`;
+    } else if (lang === "afrikaans") {
+      greeting = `Goeie dag, ${theName.charAt(0).toUpperCase() + theName.slice(1)}`;
+    } else if (lang === "xhosa") {
+      greeting = `Mholo, ${theName.charAt(0).toUpperCase() + theName.slice(1)}`;
+    }
+  }
+  function getGreeting() {
+    return greeting;
+  }
+
+  function getGreetingFor(theName) {
+    let filteredName = users.filter(option => {
+      return option.name === theName;
+    })
+
+    return filteredName[0].name;
+  }
+
+  function isNameRepeated(nameInput) {
+    let isRepeated = false;
+
+    for (let i = 0; i < users.length; i++) {
+      let currentName = users[i];
+      let name = currentName.name;
+
+      if (usersGreeted[name] === undefined) {
+        usersGreeted[name] = 0;
+      } else {
+        usersGreeted[name]++;
       }
-   }
 
-   function getUserCounter(theName) {
-      let filteredName = users.filter( option => {
-         return option.name === theName;
-      })
-
-      return filteredName[0].counter;
-   }
-
-   function getGreetingsCounter() {
-      return greetingsCounter;
-   }
-
-   function setGreeting(lang, theName) {
-      if(lang === "english") {
-        greeting = `Hello, ${theName.charAt(0).toUpperCase() + theName.slice(1)}`;
-      } else if(lang === "afrikaans") {
-         greeting = `Goeie dag, ${theName.charAt(0).toUpperCase() + theName.slice(1)}`;
-      } else if(lang === "xhosa") {
-         greeting = `Mholo, ${theName.charAt(0).toUpperCase() + theName.slice(1)}`;
+      for (let key in usersGreeted) {
+        if (usersGreeted.hasOwnProperty(nameInput)) {
+          isRepeated = true;
+        }
       }
-   }
-   function getGreeting() {
-      return greeting;
-   }
+    }
+    return isRepeated;
+  }
 
-   function getGreetingFor(theName) {
-      let filteredName = users.filter( option => {
-         return option.name === theName;
-      })
-
-      return filteredName[0].name;
-   }
-
-   function isNameRepeated(nameInput) {
-      let isRepeated  = false;
-      
-      for(let i = 0; i < users.length; i++) {
-         let currentName = users[i];
-         let name = currentName.name;
-
-         if (usersGreeted[name] === undefined){
-            usersGreeted[name] = 0;
-         } else {
-            usersGreeted[name]++;
-         }
-
-         for(let key in usersGreeted) {
-            if(usersGreeted.hasOwnProperty(nameInput)) {
-               isRepeated  = true;
-            } 
-         }   
-      } 
-      return isRepeated;
-   }
-
-   return {
-      setUser,
-      getUser,
-      getUsers,
-      increaseCounter,
-      getGreetingsCounter,
-      getUserCounter,
-      setGreeting,
-      getGreeting,
-      isNameRepeated,
-      getGreetingFor,
-      updateUserCounter
-   }
+  return {
+    setUser,
+    getUser,
+    getUsers,
+    increaseCounter,
+    getGreetingsCounter,
+    getUserCounter,
+    setGreeting,
+    getGreeting,
+    isNameRepeated,
+    getGreetingFor,
+    updateUserCounter
+  }
 }
--- test/greetings.tests.js
+++ test/greetings.tests.js
@@ -1,96 +1,165 @@
 const assert = require('assert');
 const CreateGreetings = require('../createGreetings');
 
-describe("Greetings Function", function() {
-    it('should return "2" when counter is incremented twice from zero', function(){
-        var greetInstance = CreateGreetings();
-        // let arr = [ 
-        //     { name: 'thando', counter: 1 },
-        //     { name: 'john', counter: 1 },
-        //     { name: 'busi', counter: 1 },
-        //     { name: 'andre', counter: 1 } 
-        // ]
-      
+describe("Greetings Function", function () {
+  it('should be able to set and return the list of users', function(){
+      var greetInstance = CreateGreetings();
 
-        greetInstance.setUser('thando');
-        greetInstance.setUser('john');
-        greetInstance.setUser('busi');
-        greetInstance.setUser('andre');
-        greetInstance.setUser('thando');
+      greetInstance.setUser('thando');
+      greetInstance.setUser('john');
+      greetInstance.setUser('busi');
+      greetInstance.setUser('andre');
+     
+      let results = [ 
+        { name: 'thando', counter: 1 },
+        { name: 'john', counter: 1 },
+        { name: 'busi', counter: 1 },
+        { name: 'andre', counter: 1 } 
+      ]
 
-        greetInstance.updateUserCounter('thando')
+      assert.deepEqual(greetInstance.getUsers(), results);
+  });
 
-        
-        console.log(greetInstance.isNameRepeated('bus'));
-        console.log(greetInstance.getUsers())
-        // greetInstance.setName('john');
-        // greetInstance.setName('busi');
-        // greetInstance.setName('andre');
-       
-        // console.log(greetInstance.getNames())
-        // console.log(greetInstance.getGreetingFor('thando'))
-        //greetInstance.setCounter(0);
-        // greetInstance.increaseCounter();
-        // greetInstance.increaseCounter();
-        assert.equal(2, 2);
-      
-    });
+  it('should be able to greet the user with a seleted language', function(){
+    var greetInstance = CreateGreetings();
 
-    // it('should return "Thanduxolo" and the object "{name: "Thanduxolo"}" when the string "Thanduxolo is privided as input"', function(){
-    //     var greetInstance = createGreetings();
+    greetInstance.setGreeting('english', 'thando')
 
-    //     greetInstance.setName("Thanduxolo");
-       
+    assert.equal(greetInstance.getGreeting(), 'Hello, Thando');
+  });
 
-    //     // assert.equal(greetInstance.getName(), "Thanduxolo");
-    //     // assert.deepEqual(greetInstance.getNameObj(), {name: 'Thanduxolo'} );
-    // });
+  it('should be able to return the name for a specific user in the users list', function(){
+    var greetInstance = CreateGreetings();
 
-    // it('should return "Hello, Gorge" when english button is selected', function(){
-    //     var greetInstance = createGreetings();
+    greetInstance.setUser('thando');
+    greetInstance.setUser('john');
+    greetInstance.setUser('busi');
+    greetInstance.setUser('andre');
 
-    //     greetInstance.setName("Gorge");
-    //     assert.equal(greetInstance.englishGreeting(), 'Hello, Gorge');
-    // });
+    assert.equal(greetInstance.getGreetingFor('john'), 'john');
+  });
+
+  it('should return true if the input value is already in the users list', function(){
+    var greetInstance = CreateGreetings();
+
+    greetInstance.setUser('thando');
+    greetInstance.setUser('john');
+    greetInstance.setUser('busi');
+    greetInstance.setUser('andre');
+
+    assert.equal(greetInstance.isNameRepeated('busi'), true);
+  });
+
+  it('should return false if the input value is NOT in the users list', function(){
+    var greetInstance = CreateGreetings();
+
+    greetInstance.setUser('thando');
+    greetInstance.setUser('john');
+    greetInstance.setUser('busi');
+    greetInstance.setUser('andre');
+
+    assert.equal(greetInstance.isNameRepeated('Sbu'), false);
+  });
+
+  it('should be able to update and return the number of greets for a specific user', function(){
+    var greetInstance = CreateGreetings();
+
+    greetInstance.setUser('thando');
+
+    greetInstance.updateUserCounter('thando')
+
+    assert.equal(greetInstance.getUserCounter('thando'), 2);
+  });
+
+  // it('should return "2" when counter is incremented twice from zero', function () {
+  //   var greetInstance = CreateGreetings();
+  //   // let arr = [ 
+  //   //     { name: 'thando', counter: 1 },
+  //   //     { name: 'john', counter: 1 },
+  //   //     { name: 'busi', counter: 1 },
+  //   //     { name: 'andre', counter: 1 } 
+  //   // ]
 
 
-    // it('should return "Hallo, Timothy" when afrikaans button is selected', function(){
-    //     var greetInstance = createGreetings();
+  //   greetInstance.setUser('thando');
+  //   greetInstance.setUser('john');
+  //   greetInstance.setUser('busi');
+  //   greetInstance.setUser('andre');
+  //   greetInstance.setUser('thando');
 
-    //     greetInstance.setName("Timothy");
-    //     assert.equal(greetInstance.afrikaansGreeting(), 'Hallo, Timothy');
-    // });
+  //   greetInstance.updateUserCounter('thando')
 
-    // it('should return "Molo, James" when isiXhosa button is selected', function(){
-    //     var greetInstance = createGreetings();
 
-    //     greetInstance.setName("James");
-    //     assert.equal(greetInstance.xhosaGreeting(), 'Molo, James');
-    // });
+  //   console.log(greetInstance.isNameRepeated('bus'));
+  //   console.log(greetInstance.getUsers())
+  //   // greetInstance.setName('john');
+  //   // greetInstance.setName('busi');
+  //   // greetInstance.setName('andre');
 
-    // it('should return true when a number or an empty string is provided as input and false if a valid string provided', function(){
-    //     var greetInstance = createGreetings();
+  //   // console.log(greetInstance.getNames())
+  //   // console.log(greetInstance.getGreetingFor('thando'))
+  //   //greetInstance.setCounter(0);
+  //   // greetInstance.increaseCounter();
+  //   // greetInstance.increaseCounter();
+  //   assert.equal(2, 2);
 
-    //     assert.equal(greetInstance.displayError("zanele"), false);
-    //     assert.equal(greetInstance.displayError(1234), true);
-    //     assert.equal(greetInstance.displayError(""), true);
-    // }); 
+  // });
 
-    // it('should return false when input name does not already exist on the list', function(){
-    //     var greetInstance = createGreetings();
+  // it('should return "Thanduxolo" and the object "{name: "Thanduxolo"}" when the string "Thanduxolo is privided as input"', function(){
+  //     var greetInstance = createGreetings();
 
-    //     greetInstance.setName("Johno");
-    //     var names = [{name: 'kiro'}, {name: 'tedo'},{name: 'zan'}];
+  //     greetInstance.setName("Thanduxolo");
 
-    //     assert.equal(greetInstance.checkName(names), false);
-    // }); 
 
-    // it('should return true when input name already exist on the list', function(){
-    //     var greetInstance = createGreetings();
+  //     // assert.equal(greetInstance.getName(), "Thanduxolo");
+  //     // assert.deepEqual(greetInstance.getNameObj(), {name: 'Thanduxolo'} );
+  // });
 
-    //     greetInstance.setName("zan");
-    //     var names = [{name: 'kiro'}, {name: 'tedo'},{name: 'zan'}];
+  // it('should return "Hello, Gorge" when english button is selected', function(){
+  //     var greetInstance = createGreetings();
 
-    //     assert.equal(greetInstance.checkName(names), true);
-    // }); 
+  //     greetInstance.setName("Gorge");
+  //     assert.equal(greetInstance.englishGreeting(), 'Hello, Gorge');
+  // });
+
+
+  // it('should return "Hallo, Timothy" when afrikaans button is selected', function(){
+  //     var greetInstance = createGreetings();
+
+  //     greetInstance.setName("Timothy");
+  //     assert.equal(greetInstance.afrikaansGreeting(), 'Hallo, Timothy');
+  // });
+
+  // it('should return "Molo, James" when isiXhosa button is selected', function(){
+  //     var greetInstance = createGreetings();
+
+  //     greetInstance.setName("James");
+  //     assert.equal(greetInstance.xhosaGreeting(), 'Molo, James');
+  // });
+
+  // it('should return true when a number or an empty string is provided as input and false if a valid string provided', function(){
+  //     var greetInstance = createGreetings();
+
+  //     assert.equal(greetInstance.displayError("zanele"), false);
+  //     assert.equal(greetInstance.displayError(1234), true);
+  //     assert.equal(greetInstance.displayError(""), true);
+  // }); 
+
+  // it('should return false when input name does not already exist on the list', function(){
+  //     var greetInstance = createGreetings();
+
+  //     greetInstance.setName("Johno");
+  //     var names = [{name: 'kiro'}, {name: 'tedo'},{name: 'zan'}];
+
+  //     assert.equal(greetInstance.checkName(names), false);
+  // }); 
+
+  // it('should return true when input name already exist on the list', function(){
+  //     var greetInstance = createGreetings();
+
+  //     greetInstance.setName("zan");
+  //     var names = [{name: 'kiro'}, {name: 'tedo'},{name: 'zan'}];
+
+  //     assert.equal(greetInstance.checkName(names), true);
+  // }); 
 });
